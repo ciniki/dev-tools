009c40f09dd58f9ba1bc8a9ec86163490b68d86c
Andrew Rivett <veggiefrog@gmail.com>
2013-03-18 10:45:19 -0400

    Added newsaggregator modules

--- commit.sh
+++ commit.sh
@@ -3,4 +3,4 @@
 git submodule foreach "git commit -am '$1' || :"
 git commit -am "$1"
 
-./versions.pl -i >site/_versions.ini
+#./versions.pl -i >site/_versions.ini
--- dev-init.sh
+++ dev-init.sh
@@ -17,6 +17,7 @@ cd site/ciniki-api/images; git checkout master; git remote add push git@github.c
 cd site/ciniki-api/journal; git checkout master; git remote add push git@github.com:ciniki/api-journal.git; cd ../../..
 cd site/ciniki-api/links; git checkout master; git remote add push git@github.com:ciniki/api-links.git; cd ../../..
 cd site/ciniki-api/media; git checkout master; git remote add push git@github.com:ciniki/api-media.git; cd ../../..
+cd site/ciniki-api/newsaggregator; git checkout master; git remote add push git@github.com:ciniki/api-newsaggregator.git; cd ../../..
 cd site/ciniki-api/products; git checkout master; git remote add push git@github.com:ciniki/api-products.git; cd ../../..
 cd site/ciniki-api/projects; git checkout master; git remote add push git@github.com:ciniki/api-projects.git; cd ../../..
 cd site/ciniki-api/questions; git checkout master; git remote add push git@github.com:ciniki/api-questions.git; cd ../../..
@@ -44,6 +45,7 @@ cd site/ciniki-manage/help; git checkout master; git remote add push git@github.
 cd site/ciniki-manage/journal; git checkout master; git remote add push git@github.com:ciniki/manage-journal.git; cd ../../..
 cd site/ciniki-manage/links; git checkout master; git remote add push git@github.com:ciniki/manage-links.git; cd ../../..
 cd site/ciniki-manage/media; git checkout master; git remote add push git@github.com:ciniki/manage-media.git; cd ../../..
+cd site/ciniki-manage/newsaggregator; git checkout master; git remote add push git@github.com:ciniki/manage-newsaggregator.git; cd ../../..
 cd site/ciniki-manage/monitoring; git checkout master; git remote add push git@github.com:ciniki/manage-monitoring.git; cd ../../..
 cd site/ciniki-manage/products; git checkout master; git remote add push git@github.com:ciniki/manage-products.git; cd ../../..
 cd site/ciniki-manage/projects; git checkout master; git remote add push git@github.com:ciniki/manage-projects.git; cd ../../..
