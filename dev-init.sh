b60cba9e20bb78b86c223a14b61bb8d1a344ca09
Andrew Rivett <veggiefrog@gmail.com>
2013-11-13 09:26:08 -0500

    Added pos module

--- dev-init.sh
+++ dev-init.sh
@@ -21,6 +21,7 @@ cd site/ciniki-api/links; git checkout master; git remote add push git@github.co
 cd site/ciniki-api/mail; git checkout master; git remote add push git@github.com:ciniki/api-mail.git; cd ../../..
 cd site/ciniki-api/newsaggregator; git checkout master; git remote add push git@github.com:ciniki/api-newsaggregator.git; cd ../../..
 cd site/ciniki-api/newsletters; git checkout master; git remote add push git@github.com:ciniki/api-newsletters.git; cd ../../..
+cd site/ciniki-api/pos; git checkout master; git remote add push git@github.com:ciniki/api-pos.git; cd ../../..
 cd site/ciniki-api/products; git checkout master; git remote add push git@github.com:ciniki/api-products.git; cd ../../..
 cd site/ciniki-api/projects; git checkout master; git remote add push git@github.com:ciniki/api-projects.git; cd ../../..
 cd site/ciniki-api/recipes; git checkout master; git remote add push git@github.com:ciniki/api-recipes.git; cd ../../..
@@ -54,6 +55,7 @@ cd site/ciniki-manage/mail; git checkout master; git remote add push git@github.
 cd site/ciniki-manage/newsaggregator; git checkout master; git remote add push git@github.com:ciniki/manage-newsaggregator.git; cd ../../..
 cd site/ciniki-manage/newsletters; git checkout master; git remote add push git@github.com:ciniki/manage-newsletters.git; cd ../../..
 cd site/ciniki-manage/monitoring; git checkout master; git remote add push git@github.com:ciniki/manage-monitoring.git; cd ../../..
+cd site/ciniki-manage/pos; git checkout master; git remote add push git@github.com:ciniki/manage-pos.git; cd ../../..
 cd site/ciniki-manage/products; git checkout master; git remote add push git@github.com:ciniki/manage-products.git; cd ../../..
 cd site/ciniki-manage/projects; git checkout master; git remote add push git@github.com:ciniki/manage-projects.git; cd ../../..
 cd site/ciniki-manage/recipes; git checkout master; git remote add push git@github.com:ciniki/manage-recipes.git; cd ../../..
