423d0e3ae65936a9ceb15ab20b5ee56c2a73083b
Andrew Rivett <veggiefrog@gmail.com>
2013-11-14 10:49:10 -0500

    renamed pos to sapos

--- dev-init.sh
+++ dev-init.sh
@@ -21,10 +21,10 @@ cd site/ciniki-api/links; git checkout master; git remote add push git@github.co
 cd site/ciniki-api/mail; git checkout master; git remote add push git@github.com:ciniki/api-mail.git; cd ../../..
 cd site/ciniki-api/newsaggregator; git checkout master; git remote add push git@github.com:ciniki/api-newsaggregator.git; cd ../../..
 cd site/ciniki-api/newsletters; git checkout master; git remote add push git@github.com:ciniki/api-newsletters.git; cd ../../..
-cd site/ciniki-api/pos; git checkout master; git remote add push git@github.com:ciniki/api-pos.git; cd ../../..
 cd site/ciniki-api/products; git checkout master; git remote add push git@github.com:ciniki/api-products.git; cd ../../..
 cd site/ciniki-api/projects; git checkout master; git remote add push git@github.com:ciniki/api-projects.git; cd ../../..
 cd site/ciniki-api/recipes; git checkout master; git remote add push git@github.com:ciniki/api-recipes.git; cd ../../..
+cd site/ciniki-api/sapos; git checkout master; git remote add push git@github.com:ciniki/api-sapos.git; cd ../../..
 cd site/ciniki-api/services; git checkout master; git remote add push git@github.com:ciniki/api-services.git; cd ../../..
 cd site/ciniki-api/subscriptions; git checkout master; git remote add push git@github.com:ciniki/api-subscriptions.git; cd ../../..
 cd site/ciniki-api/surveys; git checkout master; git remote add push git@github.com:ciniki/api-surveys.git; cd ../../..
@@ -55,10 +55,10 @@ cd site/ciniki-manage/mail; git checkout master; git remote add push git@github.
 cd site/ciniki-manage/newsaggregator; git checkout master; git remote add push git@github.com:ciniki/manage-newsaggregator.git; cd ../../..
 cd site/ciniki-manage/newsletters; git checkout master; git remote add push git@github.com:ciniki/manage-newsletters.git; cd ../../..
 cd site/ciniki-manage/monitoring; git checkout master; git remote add push git@github.com:ciniki/manage-monitoring.git; cd ../../..
-cd site/ciniki-manage/pos; git checkout master; git remote add push git@github.com:ciniki/manage-pos.git; cd ../../..
 cd site/ciniki-manage/products; git checkout master; git remote add push git@github.com:ciniki/manage-products.git; cd ../../..
 cd site/ciniki-manage/projects; git checkout master; git remote add push git@github.com:ciniki/manage-projects.git; cd ../../..
 cd site/ciniki-manage/recipes; git checkout master; git remote add push git@github.com:ciniki/manage-recipes.git; cd ../../..
+cd site/ciniki-manage/sapos; git checkout master; git remote add push git@github.com:ciniki/manage-sapos.git; cd ../../..
 cd site/ciniki-manage/services; git checkout master; git remote add push git@github.com:ciniki/manage-services.git; cd ../../..
 cd site/ciniki-manage/subscriptions; git checkout master; git remote add push git@github.com:ciniki/manage-subscriptions.git; cd ../../..
 cd site/ciniki-manage/surveys; git checkout master; git remote add push git@github.com:ciniki/manage-surveys.git; cd ../../..
