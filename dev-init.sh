897e85fa96dbc40995aaa8b0416a62a3c9fc2268
Andrew Rivett <veggiefrog@gmail.com>
2014-05-15 19:21:16 -0400

    Added marketing modules

--- dev-init.sh
+++ dev-init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd site/ciniki-mods/artcatalog; git checkout master; git remote add push git@github.com:ciniki/artcatalog.git; cd ../../..
-cd site/ciniki-mods/artclub; git checkout master; git remote add push git@github.com:ciniki/artclub.git; cd ../../..
 cd site/ciniki-mods/artgallery; git checkout master; git remote add push git@github.com:ciniki/artgallery.git; cd ../../..
 cd site/ciniki-mods/atdo; git checkout master; git remote add push git@github.com:ciniki/atdo.git; cd ../../..
 cd site/ciniki-mods/blog; git checkout master; git remote add push git@github.com:ciniki/blog.git; cd ../../..
@@ -13,6 +12,7 @@ cd site/ciniki-mods/core; git checkout master; git remote add push git@github.co
 cd site/ciniki-mods/courses; git checkout master; git remote add push git@github.com:ciniki/courses.git; cd ../../..
 cd site/ciniki-mods/cron; git checkout master; git remote add push git@github.com:ciniki/cron.git; cd ../../..
 cd site/ciniki-mods/customers; git checkout master; git remote add push git@github.com:ciniki/customers.git; cd ../../..
+cd site/ciniki-mods/directory; git checkout master; git remote add push git@github.com:ciniki/directory.git; cd ../../..
 cd site/ciniki-mods/events; git checkout master; git remote add push git@github.com:ciniki/events.git; cd ../../..
 cd site/ciniki-mods/exhibitions; git checkout master; git remote add push git@github.com:ciniki/exhibitions.git; cd ../../..
 cd site/ciniki-mods/filedepot; git checkout master; git remote add push git@github.com:ciniki/filedepot.git; cd ../../..
@@ -21,6 +21,7 @@ cd site/ciniki-mods/images; git checkout master; git remote add push git@github.
 cd site/ciniki-mods/info; git checkout master; git remote add push git@github.com:ciniki/info.git; cd ../../..
 cd site/ciniki-mods/links; git checkout master; git remote add push git@github.com:ciniki/links.git; cd ../../..
 cd site/ciniki-mods/mail; git checkout master; git remote add push git@github.com:ciniki/mail.git; cd ../../..
+cd site/ciniki-mods/marketing; git checkout master; git remote add push git@github.com:ciniki/marketing.git; cd ../../..
 cd site/ciniki-mods/newsaggregator; git checkout master; git remote add push git@github.com:ciniki/newsaggregator.git; cd ../../..
 cd site/ciniki-mods/newsletters; git checkout master; git remote add push git@github.com:ciniki/newsletters.git; cd ../../..
 cd site/ciniki-mods/products; git checkout master; git remote add push git@github.com:ciniki/products.git; cd ../../..
@@ -28,6 +29,7 @@ cd site/ciniki-mods/projects; git checkout master; git remote add push git@githu
 cd site/ciniki-mods/recipes; git checkout master; git remote add push git@github.com:ciniki/recipes.git; cd ../../..
 cd site/ciniki-mods/sapos; git checkout master; git remote add push git@github.com:ciniki/sapos.git; cd ../../..
 cd site/ciniki-mods/services; git checkout master; git remote add push git@github.com:ciniki/services.git; cd ../../..
+cd site/ciniki-mods/sponsors; git checkout master; git remote add push git@github.com:ciniki/sponsors.git; cd ../../..
 cd site/ciniki-mods/subscriptions; git checkout master; git remote add push git@github.com:ciniki/subscriptions.git; cd ../../..
 cd site/ciniki-mods/surveys; git checkout master; git remote add push git@github.com:ciniki/surveys.git; cd ../../..
 cd site/ciniki-mods/sysadmin; git checkout master; git remote add push git@github.com:ciniki/sysadmin.git; cd ../../..
